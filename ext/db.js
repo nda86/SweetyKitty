34899ddb82404865f8254f39183d596dc0fdcd9a
nda86 <nda86@mail.ru>
2015-04-05 02:57:04 +0800

    remove http for google fonts

--- ext/db.js
+++ ext/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-// var mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost/kittyDB'
-var mongoUri = "mongodb://nda86:password171086@ds031108.mongolab.com:31108/kittydb";
+var mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost/kittyDB'
+// var mongoUri = "mongodb://nda86:password171086@ds031108.mongolab.com:31108/kittydb";
 var db = mongoose.connect(mongoUri);
 module.exports = db;
